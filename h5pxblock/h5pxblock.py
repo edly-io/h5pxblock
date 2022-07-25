cb1b893df7a1e89e21f38f510a11ec8e9301427f
zia.fazal@arbisoft.com <zia.fazal@arbisoft.com>
2022-07-25 12:56:03 +0500

    feat: Ability to store H5P content locally
    fix: removed debugger and conver block id to str

--- h5pxblock/h5pxblock.py
+++ h5pxblock/h5pxblock.py
@@ -1,19 +1,11 @@
 """XBlock to play H5P content in open edX."""
 
-import concurrent.futures
 import json
-import hashlib
-import mimetypes
-import re
 import os
-import stat
 import logging
 import pkg_resources
-import shutil
-import xml.etree.ElementTree as ET
 
 from django.conf import settings
-from django.core.files.storage import default_storage
 from django.template import Context, Template
 from django.utils import timezone
 from webob import Response
@@ -21,9 +13,10 @@ from webob import Response
 from xblock.completable import CompletableXBlockMixin
 from xblock.core import XBlock
 from xblock.exceptions import JsonHandlerError
-from xblock.fields import Scope, String, Float, Boolean, Dict, DateTime, Integer, UNIQUE_ID
+from xblock.fields import Scope, String, Boolean, Integer, Dict, UNIQUE_ID
 from xblock.fragment import Fragment
-from xblockutils.studio_editable import StudioEditableXBlockMixin
+
+from h5pxblock.utils import str2bool, unpack_package
 
 
 # Make '_' a no-op so we can scrape strings
@@ -33,11 +26,10 @@ log = logging.getLogger(__name__)
 
 H5P_ROOT = os.path.join(settings.MEDIA_ROOT, "h5pxblockmedia")
 H5P_URL = os.path.join(settings.MEDIA_URL, "h5pxblockmedia")
-MAX_WORKERS = getattr(settings, "THREADPOOLEXECUTOR_MAX_WORKERS", 10)
 
 
-@XBlock.needs('user')
-class H5PPlayerXBlock(StudioEditableXBlockMixin, XBlock, CompletableXBlockMixin):
+@XBlock.wants('user')
+class H5PPlayerXBlock(XBlock, CompletableXBlockMixin):
     """
     H5P XBlock provides ability to host and play h5p content inside open edX course.
     It also provides ability to route xAPI event to LRS.
@@ -57,7 +49,8 @@ class H5PPlayerXBlock(StudioEditableXBlockMixin, XBlock, CompletableXBlockMixin)
     )
 
     h5p_content_json_path = String(
-        help=_("Path of h5p content root json"),
+        display_name=_("Uploade H5P content"),
+        help=_("Upload H5P content"),
         default="/resource/h5pxblock/public/js/samples/multiple-choice-713",
         scope=Scope.settings,
     )
@@ -90,24 +83,77 @@ class H5PPlayerXBlock(StudioEditableXBlockMixin, XBlock, CompletableXBlockMixin)
         scope=Scope.settings
     )
 
+    is_scorable = Boolean(
+        display_name=_("Is Scorable"),
+        help=_(
+            "Select True if this component will save score"
+        ),
+        default=False,
+        scope=Scope.settings,
+    )
+
+    save_freq = Integer(
+        display_name=_("User content state save frequency"),
+        help=_(
+            "How often current user content state should be autosaved (in seconds). Set it to zero if you don't want to save content state."
+        ),
+        default=0,
+        scope=Scope.settings,
+    )
+
     interaction_data = Dict(
         help=_("User previous content interaction states"),
         default={},
         scope=Scope.user_state
     )
 
+    h5p_content_meta = Dict(scope=Scope.content)
     has_author_view = True
-    editable_fields = (
-        'display_name', 'show_frame', 'show_copyright', 'show_h5p', 'show_fullscreen', 'h5p_content_json_path',
-    )
 
     def resource_string(self, path):
         """Handy helper for getting resources from our kit."""
         data = pkg_resources.resource_string(__name__, path)
         return data.decode("utf8")
 
+    def render_template(self, template_path, context):
+        template_str = self.resource_string(template_path)
+        template = Template(template_str)
+        return template.render(Context(context))
+
+    @property
+    def h5p_content_url(self):
+        return "{}/{}".format(H5P_URL, self.scope_ids.usage_id)
+
+    @property
+    def local_storage_path(self):
+        return os.path.join(
+            H5P_ROOT, str(self.scope_ids.usage_id)
+        )
+
+    def get_context_studio(self):
+        return {
+            "field_display_name": self.fields["display_name"],
+            "h5p_content_json_path": self.fields["h5p_content_json_path"],
+            "show_frame": self.fields["show_frame"],
+            "show_copyright": self.fields["show_copyright"],
+            "show_h5p": self.fields["show_h5p"],
+            "show_fullscreen": self.fields["show_fullscreen"],
+            "is_scorable": self.fields["is_scorable"],
+            "save_freq": self.fields["save_freq"],
+            "h5p_xblock": self,
+        }
+
     def author_view(self, context):
         return self.student_view(context)
+
+    def studio_view(self, context=None):
+        context_html = self.get_context_studio()
+        template = self.render_template("static/html/studio.html", context_html)
+        frag = Fragment(template)
+        frag.add_css(self.resource_string("static/css/studio.css"))
+        frag.add_javascript(self.resource_string("static/js/src/studio.js"))
+        frag.initialize_js("H5PStudioXBlock")
+        return frag
 
     def student_view(self, context=None):
         """
@@ -120,6 +166,7 @@ class H5PPlayerXBlock(StudioEditableXBlockMixin, XBlock, CompletableXBlockMixin)
         frag.add_javascript(self.resource_string("static/js/src/h5pxblock.js"))
         user_service = self.runtime.service(self, 'user')
         user = user_service.get_current_user()
+        save_freq = self.save_freq if self.save_freq > 0 else False
         frag.initialize_js(
             'H5PPlayerXBlock',
             json_args={
@@ -129,6 +176,7 @@ class H5PPlayerXBlock(StudioEditableXBlockMixin, XBlock, CompletableXBlockMixin)
                 "copyright": self.show_copyright,
                 "icon": self.show_h5p,
                 "fullScreen": self.show_fullscreen,
+                "saveFreq": save_freq,
                 "user_full_name": user.full_name,
                 "user_email": user.emails[0],
                 "userData": json.dumps(self.interaction_data),
@@ -152,14 +200,65 @@ class H5PPlayerXBlock(StudioEditableXBlockMixin, XBlock, CompletableXBlockMixin)
 
         return Response(json.dumps(self.interaction_data))
 
+    @XBlock.handler
+    def studio_submit(self, request, suffix=""):
+        self.display_name = request.params["display_name"]
+        self.show_frame = str2bool(request.params["show_frame"])
+        self.show_copyright = str2bool(request.params["show_copyright"])
+        self.show_h5p = str2bool(request.params["show_h5p"])
+        self.show_fullscreen = str2bool(request.params["show_fullscreen"])
+        self.is_scorable = str2bool(request.params["is_scorable"])
+        self.save_freq = request.params["save_freq"]
+        self.icon_class = "problem" if self.is_scorable else "h5p"
+
+        if hasattr(request.params["h5p_content_bundle"], "file"):
+            h5p_package = request.params["h5p_content_bundle"].file
+
+            meta_data = {
+                "name": h5p_package.name,
+                "upload_time": timezone.now(),
+                "size": h5p_package.size,
+            }
+            self.h5p_content_meta = meta_data
+            unpack_package(h5p_package, self.local_storage_path)
+            self.h5p_content_json_path = self.h5p_content_url
+
+        return Response(
+            json.dumps({"result": "success"}),
+            content_type="application/json",
+            charset="utf8",
+        )
 
     @XBlock.json_handler
-    def finish_handler(self, data, suffix=''):
+    def result_handler(self, data, suffix=''):
         """
-        Handler to injest results when h5p content triggers finish event
+        Handler to injest results when h5p content triggers completion or rescorable events
         """
-        # TODO:
-        return {"data": data}
+        save_completion, save_score = False, False
+        try:
+            self.emit_completion(1.0)
+            save_completion = True
+        except BaseException as exp:
+            log.error("Error while marking completion %s", exp)
+
+        try:
+            if self.is_scorable and data['result'] and data['result']['score']:
+                grade_dict = {
+                    'value': data['result']['score']['raw'],
+                    'max_value': data['result']['score']['max'],
+                    'only_if_higher': True,
+                }
+                self.runtime.publish(self, 'grade', grade_dict)
+                save_score = True
+        except BaseException as exp:
+            log.error("Error while publishing score %s", exp)
+
+        return Response(
+            json.dumps({"result": {"save_completion": save_completion, "save_score": save_score}}),
+            content_type="application/json",
+            charset="utf8",
+        )
+
 
     # TO-DO: change this to create the scenarios you'd like to see in the
     # workbench while developing your XBlock.
